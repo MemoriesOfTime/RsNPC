3a8eaaf998e675886d2d928af6acaafd540d19b4
Mcayear <mcayear@163.com>
2025-05-19 12:32:01 +0800

    misc: some method.

--- src/main/java/com/smallaswater/npc/RsNPC.java
+++ src/main/java/com/smallaswater/npc/RsNPC.java
@@ -44,7 +44,7 @@ public class RsNPC extends PluginBase {
             new ThreadPoolExecutor.DiscardPolicy());
     public static final Random RANDOM = new Random();
 
-    public static final String VERSION = "2.4.4";
+    public static final String VERSION = "2.5.1-SNAPSHOT";
 
     private static RsNPC rsNPC;
 
--- src/main/java/com/smallaswater/npc/command/RsNPCCommandSender.java
+++ src/main/java/com/smallaswater/npc/command/RsNPCCommandSender.java
@@ -4,6 +4,7 @@ import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.entity.Entity;
+import cn.nukkit.lang.CommandOutputContainer;
 import cn.nukkit.lang.TextContainer;
 import cn.nukkit.level.Location;
 import cn.nukkit.level.Position;
@@ -43,6 +44,10 @@ public class RsNPCCommandSender implements CommandSender {
 
     @Override
     public void sendMessage(TextContainer message) {
+    }
+
+    @Override
+    public void sendCommandOutput(CommandOutputContainer commandOutputContainer) {
     }
 
     @Override
